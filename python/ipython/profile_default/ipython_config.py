48bc334f091017645ad03d73046d3ea62e5cac39
Freddie Ruxton <freddie.ruxton@procensus.com>
2021-12-22 15:19:27 +0000

    Add python config files

---
+++ python/ipython/profile_default/ipython_config.py
@@ -0,0 +1,8 @@
+c = get_config()
+
+c.InteractiveShellApp.extensions = ["autoreload"]
+c.InteractiveShellApp.exec_lines = ["%autoreload 2"]
+
+c.InteractiveShellApp.exec_files = [
+    "/home/freddieruxton/.ipython/profile_default/imports.py",
+]
