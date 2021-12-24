d21b14c10e67301b8493fddcd51716b8cddeebeb
Freddie Ruxton <ruxton.f@gmail.com>
2021-12-24 15:59:13 +0000

    Update shell initialization for conda and nvm

--- shell/setup/06_javascript.zsh
+++ shell/setup/06_javascript.zsh
@@ -1,14 +1,6 @@
 # ================================================================
 # JavaScript
 
-# # nvm
-# export NVM_DIR="$HOME/.nvm"
-# # This loads nvm
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-# # This loads nvm bash_completion
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
-
 # yarn
 # alias ysd='wd fe && yarn start:dev'
 # alias ytf='wd fe && yarn test-frontend'
