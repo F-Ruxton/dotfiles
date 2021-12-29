9592b2655a0c92f92aaefcb4b469a4ee1b67bc58
Freddie Ruxton <ruxton.f@gmail.com>
2021-12-29 12:57:54 +0000

    Make exa aliases conditional on availability of git flags

--- shell/setup/01_shell.zsh
+++ shell/setup/01_shell.zsh
@@ -26,12 +26,21 @@ v() {
     nvim
 }
 
+# git flag currently disabled in ubuntu https://github.com/ogham/exa/issues/761
+if exa --git > /dev/null 2>&1 ; then                                                                                                                         47
+  alias l='exa -lbF --git'      # list, size, type, git
+  alias ll='exa -lbGF --git'    # long list
+  alias llm='exa -lbGd --git --sort=modified'  # long list, modified date sort
+  alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
+  alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
+else
+  alias l='exa -lbF'      # list, size, type, git
+  alias ll='exa -lbGF'    # long list
+  alias llm='exa -lbGd --sort=modified'  # long list, modified date sort
+  alias la='exa -lbhHigUmuSa --time-style=long-iso --color-scale'  # all list
+  alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --color-scale' # all + extended list
+fi
 alias ls='exa'                # ls
-alias l='exa -lbF --git'      # list, size, type, git
-alias ll='exa -lbGF --git'    # long list
-alias llm='exa -lbGd --git --sort=modified'  # long list, modified date sort
-alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
-alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
 alias lS='exa -1'                 # one column, just names
 alias lt='exa --tree --level=2'   # tree
 
