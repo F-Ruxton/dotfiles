d77ef42665c51c6b1a9fffde7e33f3a7a890521d
Freddie Ruxton <ruxton.f@gmail.com>
2022-05-28 11:13:57 +0100

    move nvm activation to javascript shell setup file

--- shell/setup/06_javascript.zsh
+++ shell/setup/06_javascript.zsh
@@ -1,6 +1,10 @@
 # ================================================================
 # JavaScript
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 # yarn
 # alias ysd='wd fe && yarn start:dev'
 # alias ytf='wd fe && yarn test-frontend'
