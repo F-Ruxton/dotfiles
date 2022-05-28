2639c79528444a6fe9d1fb518cb0b50e7ea9e160
Freddie Ruxton <ruxton.f@gmail.com>
2022-05-28 11:11:52 +0100

    remove custom docker aliases

--- shell/setup/03_docker.zsh
+++ shell/setup/03_docker.zsh
@@ -2,12 +2,6 @@
 # Docker
 
 alias d="docker"
-alias dc="docker-compose"
-alias dce="docker-compose exec"
-alias dcu='docker-compose up'
-alias dcub='docker-compose up --build'
-alias dcd='docker-compose down'
-alias dcs='docker-compose stop'
 alias docker-reclaim-space='docker run --privileged --pid=host docker/desktop-reclaim-space'
 alias docker-raw-size='ls -lh ~/Library/Containers/com.docker.docker/Data/vms/0/Docker.raw'
 
