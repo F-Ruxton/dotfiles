5c5db5406edf4556cb8b96ddba51b70b6e5f74dc
Freddie Ruxton <ruxton.f@gmail.com>
2022-05-30 06:42:49 +0100

    enable dap in lvim config

--- lvim/lua/user/lvim.lua
+++ lvim/lua/user/lvim.lua
@@ -23,3 +23,4 @@ lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 0
+lvim.builtin.dap.active = true
