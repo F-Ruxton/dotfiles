013cb471ba8b840a5f788b57e22516c6cb3aef02
Freddie Ruxton <ruxton.f@gmail.com>
2022-05-30 06:43:20 +0100

    formatting in which-key config

--- lvim/lua/user/which_key.lua
+++ lvim/lua/user/which_key.lua
@@ -1,5 +1,6 @@
 -- Use which-key to add extra bindings with the leader-key prefix
 -- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+
 lvim.builtin.which_key.mappings["t"] = {
     name = "+Trouble",
     d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
@@ -17,20 +18,16 @@ lvim.builtin.which_key.mappings["S"] = {
     l = { "<cmd>lua require('persistence').load({ last = true })<cr>", "Restore last session" },
     Q = { "<cmd>lua require('persistence').stop()<cr>", "Quit without saving session" },
 }
--- Remaps for the refactoring operations currently offered by the plugin
 
+-- Remaps for the refactoring operations currently offered by the plugin
 lvim.builtin.which_key.mappings["r"] = {
     name = "refactoring",
-
     -- Visual mode mappings
     f = { "<Esc><Cmd>lua require('refactoring').refactor('Extract Function')<CR>", "Extract Funtion", mode = "v" },
     F = { "<Esc><Cmd>lua require('refactoring').refactor('Extract Function To File')<CR>", "Extract Function to File", mode = "v" },
     v = { "<Esc><Cmd>lua require('refactoring').refactor('Extract Variable')<CR>", "Extract Variable", mode = "v" },
     i = { "<Esc><Cmd>lua require('refactoring').refactor('Inline Variable')<CR>", "Inline Variable", mode = "v" },
-
-    -- Extract block doesn't need visual mode
     b = { "<Cmd>lua require('refactoring').refactor('Extract Block')<CR>", "Extract Block", mode = "v" },
     B = { "<Cmd>lua require('refactoring').refactor('Extract Block To File')<CR>", "Extract Block to File", mode = "v" },
-
     r = { "<Cmd>lua require('refactoring').select_refactor()<CR>", "Select Refactoring", mode = "v" },
 }
