260b3e5bd3b034a43d9d4346f10a1feb12240011
Freddie Ruxton <ruxton.f@gmail.com>
2022-05-30 06:52:28 +0100

    working user-extension of lvim treesitter opts

--- lvim/lua/user/treesitter.lua
+++ lvim/lua/user/treesitter.lua
@@ -1,18 +1,78 @@
--- Treesitter
--- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
     "bash",
-    "c",
-    "javascript",
     "json",
     "lua",
     "python",
-    "typescript",
-    "tsx",
-    "css",
-    "rust",
-    "java",
     "yaml",
 }
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
+
+
+local user_opts = {
+    textobjects = {
+        select = {
+            enable = true,
+            -- Automatically jump forward to textobj, similar to targets.vim
+            lookahead = true,
+            keymaps = {
+                -- You can use the capture groups defined in textobjects.scm
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ac"] = "@class.outer",
+                ["ic"] = "@class.inner",
+            },
+        },
+        swap = {
+            enable = true,
+            swap_next = {
+                ["<leader>a"] = "@parameter.inner",
+            },
+            swap_previous = {
+                ["<leader>A"] = "@parameter.inner",
+            },
+        },
+        move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+                ["]m"] = "@function.outer",
+                ["]]"] = "@class.outer",
+            },
+            goto_next_end = {
+                ["]M"] = "@function.outer",
+                ["]["] = "@class.outer",
+            },
+            goto_previous_start = {
+                ["[m"] = "@function.outer",
+                ["[["] = "@class.outer",
+            },
+            goto_previous_end = {
+                ["[M"] = "@function.outer",
+                ["[]"] = "@class.outer",
+            },
+        },
+        lsp_interop = {
+            enable = true,
+            border = 'none',
+            peek_definition_code = {
+                ["<leader>Tf"] = "@function.outer",
+                ["<leader>TF"] = "@class.outer",
+            },
+        },
+    },
+}
+
+local status_ok, treesitter_configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+    Log:error "Failed to load nvim-treesitter.configs"
+    return
+end
+
+local base_opts = vim.deepcopy(lvim.builtin.treesitter)
+
+lvim.builtin.treesitter = vim.tbl_deep_extend(
+    "force",
+    base_opts,
+    user_opts
+)
