ff4307a293917a43dab52627f94f8e6902660c01
Freddie Ruxton <ruxton.f@gmail.com>
2022-06-08 09:23:47 +0100

    add missing bash header to _common_post.sh

--- base/_common_post.sh
+++ base/_common_post.sh
@@ -1,5 +1,8 @@
+#!/usr/bin/env bash
+
 SCRIPT_DIR="$1"
 
 # install lunarvim
 bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
 mv ~/.config/lvim  "$SCRIPT_DIR/../lvim"
+
