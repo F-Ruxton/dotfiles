1f770ff4deee5fbaaa321eda467a3c5699f54ad4
Freddie Ruxton <ruxton.f@gmail.com>
2022-06-08 09:25:41 +0100

    copy in tmux configuration

---
+++ tmux/install_plugins.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -o pipefail
+
+# Install TPM if not found
+if [ ! -e "$HOME/.tmux/plugins/tpm" ]; then
+  printf "WARNING: Cannot find TPM (Tmux Plugin Manager) \
+ at default location: \$HOME/.tmux/plugins/tpm.\n"
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+# Install TPM plugins.
+# TPM requires running tmux server, as soon as `tmux start-server` does not work
+# create dump __noop session in detached mode, and kill it when plugins are installed
+printf "Install TPM plugins\n"
+tmux new -d -s __noop >/dev/null 2>&1 || true
+tmux set-environment -g TMUX_PLUGIN_MANAGER_PATH "$HOME/.tmux/plugins"
+"$HOME"/.tmux/plugins/tpm/bin/install_plugins || true
+tmux kill-session -t __noop >/dev/null 2>&1 || true
+
+printf "OK: Completed\n"
+
