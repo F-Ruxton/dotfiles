96a9fbded44b5bb71623e5357f1b992d3ca78e24
Freddie Ruxton <ruxton.f@gmail.com>
2022-07-16 16:48:47 +0100

    add config for hop.nvim

---
+++ lvim/lua/user/hop.lua
@@ -0,0 +1,12 @@
+local ok, M = pcall(require, "hop")
+
+if not ok then
+    if vim.g.debug_plugin_loaders then
+        print("Failed to load module: hop")
+    end
+    return
+end
+
+M.setup()
+vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
+vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
