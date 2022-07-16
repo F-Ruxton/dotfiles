9e364daabf55a554152b6aa64da5118b8da020f1
Freddie Ruxton <ruxton.f@gmail.com>
2022-07-16 16:45:51 +0100

    add config for autosession.nvim

---
+++ lvim/lua/user/auto-session.lua
@@ -0,0 +1,13 @@
+local ok, M = pcall(require, "auto-session")
+
+if not ok then
+    if vim.g.debug_plugin_loaders then
+        print("Failed to load module: auto-session")
+    end
+    return
+end
+
+M.setup {
+    log_level = 'info',
+    auto_session_suppress_dirs = { '~/', '~/Projects' }
+}
