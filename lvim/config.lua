e13a2ae8f1e983fce15e9a093bd02f4730e927ba
Freddie Ruxton <ruxton.f@gmail.com>
2022-07-16 16:51:55 +0100

    update configs imported in config.lua

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,13 +1,29 @@
--- lvim is the global options object
+vim.g.debug_plugin_loaders = true
 
-require("user.lvim")
--- require("user.octo")
-require("user.options")
-require("user.treesitter")
-require("user.telescope")
-require("user.which_key")
-require("user.lsp")
+-- Load plugins list and config for lvim and nvim
 require("user.plugins")
+require("user.lvim")
+require("user.options")
+
+-- Load plugin configs
+require("user.auto-session")
 require("user.commands")
 require("user.dap")
+require("user.gh")
+require("user.gitlinker")
 require("user.harpoon")
+require("user.hop")
+require("user.indent-blankline")
+require("user.lsp")
+require("user.marks")
+require("user.neogen")
+require("user.neoscroll")
+require("user.neotest")
+require("user.nvim-bqf")
+require("user.nvim-lastplace")
+require("user.telescope")
+require("user.treesitter")
+require("user.vim-surround")
+require("user.which_key")
+-- require("user.octo")
+-- require("user.persistence")
